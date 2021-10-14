c05cc2a4e43aa38fc8d47e1a038e2e5d1433683d
Alexander Kozhevnikov <mentalisttraceur@gmail.com>
2021-10-14 15:52:37 +0000

    Clean up top header comment: SPDX identifier

--- poll.c
+++ poll.c
@@ -1,22 +1,5 @@
-/*****************************************************************************\
- * poll 1.0.3
- * Copyright (C) 2016-01-23 Alexander Kozhevnikov <mentalisttraceur@gmail.com>
- * 
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public Licence as published by
- * the Free Software Foundation, either version 3 of the licence or,
- * (at your option) any later version.
- * 
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
- * GNU General Public License for details.
- * 
- * You should've received a copy of the GNU General Public License
- * with this program. If not, see <http://www.gnu.org/licences/>,
- * or write to the Free Software Foundation, Inc.,
- * 59 Temple Place, Suite 330 Boston MA 02111-1307 USA.
-\*****************************************************************************/
+/* SPDX-License-Identifier: 0BSD */
+/* Copyright 2015 Alexander Kozhevnikov <mentalisttraceur@gmail.com> */
 
 /*\
 NOTE: some poll events might not be defined when compiling with default
